1502c8ec69242fa7ca497025dc18fe06db45c7da
zhangpeng <zhangpeng@dongfangfuli.com>
2021-06-05 17:56:46 +0800

    Initial commit

---
+++ src/main/java/com/kula/Zp9Application.java
@@ -0,0 +1,13 @@
+package com.kula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zp9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zp9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kula/Zp9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zp9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
